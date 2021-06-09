a1305600bc23e50bd81b3205324e9f7d6542e37e
Oleg <olegrikov@ya.ru>
2021-06-09 09:13:31 +0300

    Initial commit

---
+++ src/test/java/com/webStorageHW/WebStorageHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webStorageHW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStorageHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
